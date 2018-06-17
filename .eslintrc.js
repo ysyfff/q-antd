7401e3f0f42d94c7ef26028cb19c45db0cbfeabf
shiyong.yin <1156546473@qq.com>
2018-06-18 01:51:28 +0800

    update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,17 @@
 const eslintrc = {
-    extends: ['eslint-config-airbnb'],
-    parser: 'babel-eslint',
-    plugins: ['react'],
-    rules: {
-        'react/prop-types': 0,
-        'no-plusplus': 0,
-        'react/sort-comp': 0,
-        'comma-dangle': ['error', 'always-multiline'],
-        'function-paren-newline': 0,
-        'object-curly-newline': 0,
-        'class-methods-use-this': 0,
-        'no-restricted-globals': 0,
-    }
-
-}
+  extends: ['eslint-config-airbnb'],
+  parser: 'babel-eslint',
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 0,
+    'no-plusplus': 0,
+    'react/sort-comp': 0,
+    'comma-dangle': ['error', 'always-multiline'],
+    'function-paren-newline': 0,
+    'object-curly-newline': 0,
+    'class-methods-use-this': 0,
+    'no-restricted-globals': 0,
+  },
+};
 
 module.exports = eslintrc;
--- tests/setup.js
+++ tests/setup.js
@@ -1,3 +1,5 @@
+/* eslint-disable global-require */
+
 const Enzyme = require('enzyme');
 
 let Adapter;
