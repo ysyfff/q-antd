601e81857894812694397afef19c0c50086bb5a9
ysy <1156546473@qq.com>
2018-07-05 00:58:36 +0800

    add test

--- component/form/__tests__/form.test.js
+++ component/form/__tests__/form.test.js
@@ -49,11 +49,11 @@ describe('测试Form', () => {
     expect(wrapper.find('label').hasClass('i-form-label')).toBe(true)
   });
 
- 
+
 
 });
 
-describe('测试Item', ()=>{
+describe('测试Item', () => {
   const model2 = observable({
     name: '你猜不到'
   })
@@ -79,5 +79,52 @@ describe('测试Item', ()=>{
   });
   it('requiredFlag support false', () => {
     expect(wrapper2.find('label').hasClass('ant-form-item-required')).toBe(false)
+  });
+
+})
+
+describe('测试Item rules', () => {
+
+  const model = observable({
+    phoneNumber: '',
+    name: '尹士勇',
+    verifyCode: '',
+    password: '',
+    passwordAgain: ''
+  });
+  const rules = observable({
+    pwd: [{
+      required: true, message: '必须填写', trigger: 'blur'
+    }],
+
+  });
+  @observer
+  class DemoValidate extends React.Component {
+    render() {
+      return (
+        <Form model={model} rules={rules}>
+          <FormItem prop="pwd">
+            <Input_ duplex="password" />
+          </FormItem>
+        </Form>
+      )
+    }
+  }
+  const wrapper = mount(
+    <DemoValidate />
+  );
+
+  it('required=true, requiredFlag=true', () => {
+    expect(wrapper.find('label').hasClass('ant-form-item-required')).toBe(true)
+  });
+  //改变属性
+  it('test required when value is not empty', () => {
+    wrapper.find('input').simulate('change', { target: { value: '23' } });
+    expect(wrapper.find('.ant-form-explain')).toHaveLength(0);
+  })
+
+  it('test required when value is empty', () => {
+    wrapper.find('input').simulate('change', { target: { value: '' } });
+    expect(wrapper.find('.ant-form-explain')).toHaveLength(1);
   })
 })
